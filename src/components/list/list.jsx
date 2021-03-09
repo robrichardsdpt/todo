a7ea577515c21d6db39676f3a9803ac3f1e53e9a
Rob <robrichardsdpt@gmail.com>
2021-03-09 08:47:22 -0500

    list component styles upgraded

--- src/components/list/list.jsx
+++ src/components/list/list.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import './list.styles.scss'
 
 const List = () => {
   return (
-    <div>
-      this is your todo list
+    <div className='list'>
+      <div className='list__header'>
+       <h2>this is your todo list</h2>
+      </div>
+      <div className='list__content'>
+        <ul>
+          <li>Item 1</li>
+          <li>Item 2</li>
+          <li>Item 3</li>
+          <li>Item 4</li>
+          <li>Item 5</li>
+          <li>Item 6</li>
+        </ul>
+      </div>
     </div>
   )
 
