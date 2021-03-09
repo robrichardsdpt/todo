3470fdfdaa784662b94d016ab3e9ece5a7eafaf4
Rob <robrichardsdpt@gmail.com>
2021-03-09 08:24:25 -0500

    added polygon effect to top;

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import './App.scss'
 const App = () => {
   return (
     <div className="app">
+      <div className='heading'>
+        <h1>
+          Todo List
+        </h1>
+      </div>
     </div>
   )
 }
