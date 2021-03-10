18dc7352be8dcbd69d8983632bfe92a012780d33
Rob <robrichardsdpt@gmail.com>
2021-03-09 20:13:30 -0500

    added map functionality on array.

--- src/components/list/list.jsx
+++ src/components/list/list.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 import './list.styles.scss'
 
 const List = () => {
+  const todos = ['Set up internet', 'build desk', 'go to ikea']
+
+  const todosJsx = todos.map((todo, index) => {
+    return(
+      <div key={index}>
+        <h2>{todo}</h2>
+      </div>
+    )
+  })
   return (
     <div className='list'>
       <div className='list__header'>
@@ -9,12 +18,7 @@ const List = () => {
       </div>
       <div className='list__content'>
         <ul>
-          <li>Item 1</li>
-          <li>Item 2</li>
-          <li>Item 3</li>
-          <li>Item 4</li>
-          <li>Item 5</li>
-          <li>Item 6</li>
+          {todosJsx}
         </ul>
       </div>
     </div>
